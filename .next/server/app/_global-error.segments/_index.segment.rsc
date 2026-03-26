d237cb7ed795a63474f8ffe08fdcb9223533474e
Tanay Sachdeva <tanaysachdeva873@gmail.com>
2026-03-26 22:22:47 +0530

    Remove .next from repo and update .gitignore

--- .next/server/app/_global-error.segments/_index.segment.rsc
+++
@@ -1,5 +0,0 @@
-1:"$Sreact.fragment"
-2:I[65802,["/_next/static/chunks/0_eb0~5dprq1k.js"],"default"]
-3:I[685,["/_next/static/chunks/0_eb0~5dprq1k.js"],"default"]
-4:[]
-0:{"rsc":["$","$1","c",{"children":[null,["$","$L2",null,{"parallelRouterKey":"children","template":["$","$L3",null,{}]}]]}],"isPartial":false,"staleTime":300,"varyParams":"$W4","buildId":"OsQr3GcGbrJDUtBWjTpfo"}
--- .next/server/chunks/ssr/0mqh_next_dist_client_components_builtin_forbidden_0jewgr8.js
+++
@@ -1,3 +0,0 @@
-module.exports=[285,(a,b,c)=>{"use strict";Object.defineProperty(c,"__esModule",{value:!0}),Object.defineProperty(c,"default",{enumerable:!0,get:function(){return f}});let d=a.r(93817),e=a.r(90260);function f(){return(0,d.jsx)(e.HTTPAccessErrorFallback,{status:403,message:"This page could not be accessed."})}("function"==typeof c.default||"object"==typeof c.default&&null!==c.default)&&void 0===c.default.__esModule&&(Object.defineProperty(c.default,"__esModule",{value:!0}),Object.assign(c.default,c),b.exports=c.default)},77391,a=>{a.n(a.i(285))}];
-
-//# sourceMappingURL=0mqh_next_dist_client_components_builtin_forbidden_0jewgr8.js.map
